a675a912da2aae8e3ccaa07242e27003bb7a38b1
Mohamed Tawfik <mtawfik@synapse-analytics.io>
2022-06-07 18:10:36 +0200

    chore(readme): add a detailed README.md file

--- build.sh
+++ build.sh
@@ -24,15 +24,15 @@ cp {Dockerfile,.dockerignore} "$KONAN_BUILD_PATH/"
 
 # Copy over app files
 mkdir -p "$KONAN_BUILD_PATH/app"
-cp {retrain.sh,requirements.txt} "$KONAN_BUILD_PATH/app/"
+cp housing-model/{retrain.sh,requirements.txt} "$KONAN_BUILD_PATH/app/"
 # Copy over app/src files
 mkdir -p "$KONAN_BUILD_PATH/app/src"
-cp -r src/app/* "$KONAN_BUILD_PATH/app/src/"
+cp -r housing-model/src/app/* "$KONAN_BUILD_PATH/app/src/"
 # Copy app/src/utils files
 mkdir -p "$KONAN_BUILD_PATH/app/src/utils"
-cp -r src/playground/utils/* "$KONAN_BUILD_PATH/app/src/utils"
+cp -r housing-model/src/playground/utils/* "$KONAN_BUILD_PATH/app/src/utils"
 
 # Copy over app/artifacts files
 mkdir -p "$KONAN_BUILD_PATH/app/artifacts"
 cp data/metadata.yml "$KONAN_BUILD_PATH/app/artifacts/metadata.yml"
-export KONAN_MODEL_REGRESSOR_NAME="$1" && export KONAN_MODEL_ARTIFACTS_PATH="$KONAN_BUILD_PATH/app/artifacts" && python -m poetry run python src/playground/train.py src/playground/train.py
+export KONAN_MODEL_REGRESSOR_NAME="$1" && export KONAN_MODEL_ARTIFACTS_PATH="$KONAN_BUILD_PATH/app/artifacts" && python housing-model/src/playground/train.py housing-model/src/playground/train.py
